8403b2018aebbf452e2c11428bfacaaf2d2ee5e3
brunoconterato <brunoconterato@gmail.com>
2024-11-10 04:06:05 -0300

    feat: add LDR sensor

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,10 +1,14 @@
-const int P_green_buttonPin = 26;
-const int K_blue_buttonPin = 22;
+#define P_green_buttonPin 26
+#define K_blue_buttonPin 22
+#define LDR_PIN 35
 
+const double rl10 = 50000.0; // LDR resistance at 10 lux
+const double ldrGamma = 0.7;
 
 void setup() {
   pinMode(P_green_buttonPin, INPUT_PULLUP);
   pinMode(K_blue_buttonPin, INPUT_PULLUP);
+  pinMode(LDR_PIN, INPUT);
 
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -28,7 +32,28 @@ void loop() {
     Serial.println("Button K Not Pressed");
   }
 
+  Serial.print("Light: " + String(read_ldr()));
+
+
   Serial.println();
 
   delay(1000);
+}
+
+double calculate_resistance(int ldr_value) {
+    double voltage_ratio = ldr_value / (4095.0 - ldr_value);
+    return 10000.0 * voltage_ratio;
+}
+
+double calculate_lux(double resistance) {
+    return 10.0 * pow(rl10 / resistance, 1.0 / ldrGamma);
+}
+
+double read_ldr() {
+    int value = analogRead(LDR_PIN);
+    double resistance = calculate_resistance(value);
+    double light_level = calculate_lux(resistance);
+    Serial.print("Luminosidade: ");
+    Serial.println(light_level);
+    return light_level;
 }
